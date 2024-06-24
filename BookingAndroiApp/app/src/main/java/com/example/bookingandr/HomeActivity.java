5c2174d30251a06922c0a016966a4fe5ef26a55a
datpthe163028 <133310687+datpthe163028@users.noreply.github.com>
2024-06-24 15:55:47 +0700

    <feature> navigation

--- BookingAndroiApp/app/src/main/java/com/example/bookingandr/HomeActivity.java
+++ BookingAndroiApp/app/src/main/java/com/example/bookingandr/HomeActivity.java
@@ -1,33 +1,37 @@
- package com.example.bookingandr;
+package com.example.bookingandr;
 
-import android.content.Intent;
 import android.os.Bundle;
-import android.os.Handler;
-import android.os.Looper;
 
 import androidx.activity.EdgeToEdge;
 import androidx.appcompat.app.AppCompatActivity;
 import androidx.core.graphics.Insets;
 import androidx.core.view.ViewCompat;
 import androidx.core.view.WindowInsetsCompat;
+import androidx.fragment.app.FragmentStatePagerAdapter;
+import androidx.viewpager.widget.ViewPager;
 
+import com.google.android.material.bottomnavigation.BottomNavigationView;
 import com.google.android.material.tabs.TabLayout;
 
- public class MainActivity extends AppCompatActivity {
+public class HomeActivity extends AppCompatActivity {
+
+    private TabLayout mTabLayout;
+    private ViewPager mviewPager;
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         EdgeToEdge.enable(this);
-        setContentView(R.layout.activity_main);
+        setContentView(R.layout.activity_home);
 
-        new Handler(Looper.getMainLooper()).postDelayed(new Runnable() {
-            @Override
-            public void run() {
-                Intent intent = new Intent(MainActivity.this, HomeActivity.class);
-                startActivity(intent);
-                finish();
-            }
-        }, 1800);
+        mTabLayout  = findViewById(R.id.tabLayout);
+        mviewPager = findViewById(R.id.viewPager);
+
+        ViewPagerAdapter viewPagerAdapter = new ViewPagerAdapter(getSupportFragmentManager(), FragmentStatePagerAdapter.BEHAVIOR_RESUME_ONLY_CURRENT_FRAGMENT);
+        mviewPager.setAdapter(viewPagerAdapter);
+
+        mTabLayout.setupWithViewPager(mviewPager);
+
 
         ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main), (v, insets) -> {
             Insets systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars());
---
+++ BookingAndroiApp/app/src/main/java/com/example/bookingandr/ViewPagerAdapter.java
@@ -0,0 +1,51 @@
+package com.example.bookingandr;
+
+import androidx.annotation.NonNull;
+import androidx.fragment.app.Fragment;
+import androidx.fragment.app.FragmentManager;
+import androidx.fragment.app.FragmentStatePagerAdapter;
+
+public class ViewPagerAdapter extends FragmentStatePagerAdapter {
+    public ViewPagerAdapter(@NonNull FragmentManager fm, int behavior) {
+        super(fm, behavior);
+    }
+
+    @NonNull
+    @Override
+    public Fragment getItem(int position) {
+        switch (position){
+            case 0:
+                return new RoomFragment();
+            case 1:
+                return new HistoryFragment();
+            case 2:
+                return new SwapRoomFragment();
+            case 3:
+                return new NewsFragment();
+
+            default:
+                return new RoomFragment();
+        }
+    }
+
+    @Override
+    public CharSequence getPageTitle(int position) {
+        switch (position) {
+            case 0:
+                return "Room";
+            case 1:
+                return "History";
+            case 2:
+                return "Swap room";
+            case 3:
+                return "News";
+            default:
+                return null;
+        }
+    }
+
+    @Override
+    public int getCount() {
+        return 4;
+    }
+}
